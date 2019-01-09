e94de8a336db1a32276335dcdd6c15f7bdd7aab5
chris-clifton <cliftoncpx@gmail.com>
2019-01-09 14:47:25 -0500

    Cleaned up comments

--- challenge.rb
+++ challenge.rb
@@ -1,9 +1,5 @@
 # challenge.rb
 class Challenge
-  # Print the number of integers in an array that are above the given input
-  # and the number that are below
-  # e.g.: array [1, 5, 2, 1, 10] with input 6, print "above: 1, below: 4"
-
   # def above_or_below(array, input)
   #   above, below = array.partition{ |num| num > input }
   #   p "above: #{above.length}, below: #{below.length}"
@@ -26,9 +22,6 @@ class Challenge
     p "above: #{above}, below: #{below}"
   end
 
-  # Rotate the characters in a string by a given input and have the overflow
-  # appear at the beginning
-  # e.g.: "MyString" rotated by 2 is "ngMyStri"
   def rotate_string(string, number)
     overflow = string.slice!(-number..-1)
     overflow << string
